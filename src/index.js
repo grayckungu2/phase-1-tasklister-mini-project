c2c6e469a99fec8f7335f6bc15401bc2df6649a8
Grace kungu <grayckungu2@gmail.com>
2023-06-26 20:57:33 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTask = document.createElement("li");
+    const taskInput = document.getElementById("new-task-description");
+    newTask.innerText = taskInput.value;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  });
+
 });
+
